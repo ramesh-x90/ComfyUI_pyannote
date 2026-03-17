f513334e94afdc59667edbcb82b0fdef068199a6
Mateusz Kurowski <pythoncontrol@gmail.com>
2026-03-17 01:56:00 +0100

    Fix token parameter name in diarize_audio method
    pyannote-audio 4.0.4

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ class SpeakerDiarizationNode:
 
     def diarize_audio(self, audio: dict, hf_token: str) -> List[Dict[str, Any]]:
 
-        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=hf_token)
+        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", token=hf_token)
         
         audio_save_path = os.path.join(os.getcwd() , "input" , f"{uuid.uuid1()}.wav")
         
